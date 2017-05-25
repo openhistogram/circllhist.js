25388776b2c8f263438077292212851dfbd3190d
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2017-05-25 14:02:37 +0000

    Actually run the tests

--- runtests.js
+++ runtests.js
@@ -1,11 +1,11 @@
 var test = require('./circllhist_test.js')
 test(
-  { ok: function() {},
+  { ok: function() { console.log("ok"); },
     notok: function(err) {
       console.log(err);
       process.exit(-2);
     }
   }
-)
+)()
 
 console.log("ok");
